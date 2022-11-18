57f1f0b5bf561da1d33e7c075b13751c2004d3c7
João Miguel Marinho da Cunha <joaocunha@Joaos-MacBook-Pro.local>
2022-11-18 10:54:16 +0000

    📦 Fix demo assets directory to support GitHub Pages

--- vite.demo.config.ts
+++ vite.demo.config.ts
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "react-headless-markdown-editor",
   build: {
     outDir: "demo",
-    assetsDir: "react-headless-markdown-editor/assets",
   },
 });
